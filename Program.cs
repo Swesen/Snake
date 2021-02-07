527e29d0cd06347ffae8d833a130b96818381eec
Alejandra lopez pineda <al.lopezpineda@gmail.com>
2021-02-07 20:46:38 +0100

    Method Input (Test)

--- Program.cs
+++ Program.cs
@@ -1,14 +1,26 @@
 ﻿using System;
+using static System.Console;
 
 namespace Snake
 {
     class Program
     {
+        ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
+        char key = 'W';
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snek!");
             Console.WriteLine("*******************************************");
             Console.WriteLine("The best game!");
+        }
+
+        public void Input()
+        {
+            if (Console.KeyAvailable)
+            {
+                keyInfo = ReadKey(true);
+                key = keyInfo.KeyChar;
+            }
         }
     }
 }
