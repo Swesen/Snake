bc741283ab5fe235ad0d16c8f63177e6a57a4795
Linus Boréus <38762425+Swesen@users.noreply.github.com>
2021-02-08 14:38:19 +0100

    Chore create comments and welcome screen (#7)
    * Comment to reduce conflicts
    
    * Create welcome screen function

--- Program.cs
+++ Program.cs
@@ -4,11 +4,33 @@ namespace Snake
 {
     class Program
     {
+        // global variables
+
         static void Main(string[] args)
         {
+            bool playAgain = false;
+            do
+            {
+                // welcome screen
+                WelcomeScreen();
+
+                // ask for player name
+
+                // run game loop
+
+                // print score
+
+                // ask play again
+
+            } while (playAgain);
+        }
+
+        private static void WelcomeScreen()
+        {
             Console.WriteLine("Welcome to Snek!");
-            Console.WriteLine("*******************************************");
-            Console.WriteLine("The best game!");
+            Console.WriteLine("****************");
+            Console.WriteLine(" The best game! ");
+            Console.ReadKey();
         }
     }
 }
