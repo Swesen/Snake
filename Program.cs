c3476c3826b306b6cdeb3a826fa986ed0677c8e9
Linus Boréus <38762425+Swesen@users.noreply.github.com>
2021-02-08 17:25:29 +0100

    Created a way of reading keypresses without pausing the game

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Snake
 {
@@ -17,6 +18,7 @@ namespace Snake
                 // ask for player name
 
                 // run game loop
+                Game(500);
 
                 // print score
 
@@ -31,6 +33,57 @@ namespace Snake
             Console.WriteLine("****************");
             Console.WriteLine(" The best game! ");
             Console.ReadKey();
+        }
+
+        // gameLoopMS controls the time between updates
+        static void Game(int gameLoopMS)
+        {
+            do
+            {
+                // start timer to keep track of execution time
+                var timer = System.Diagnostics.Stopwatch.StartNew();
+
+                // execute any game updating logick after this point
+
+
+                // after every update read user input controls untill the next game update
+                ConsoleKey input = new ConsoleKey();
+                do
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        ConsoleKeyInfo key = Console.ReadKey(true);
+
+                        // only accept keys that are used in game
+                        switch (key.Key)
+                        {
+                            case ConsoleKey.LeftArrow:
+                                input = key.Key;
+                                break;
+                            case ConsoleKey.RightArrow:
+                                input = key.Key;
+                                break;
+                            case ConsoleKey.UpArrow:
+                                input = key.Key;
+                                break;
+                            case ConsoleKey.DownArrow:
+                                input = key.Key;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                } while (timer.ElapsedMilliseconds < gameLoopMS);
+
+                // debug
+                if (input != ConsoleKey.Escape)
+                {
+                    Console.Write(input.ToString());
+                }
+
+                timer.Stop();
+
+            } while (true);
         }
     }
 }
