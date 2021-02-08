b52319113fc02fb99d32c34aa0dcac943c72a22b
Linus Boréus <38762425+Swesen@users.noreply.github.com>
2021-02-08 14:34:02 +0100

    Comment to reduce conflicts

--- Program.cs
+++ Program.cs
@@ -4,11 +4,28 @@ namespace Snake
 {
     class Program
     {
+        // global variables
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snek!");
-            Console.WriteLine("*******************************************");
-            Console.WriteLine("The best game!");
+            bool playAgain = false;
+            do
+            {
+
+                // welcome screen
+                Console.WriteLine("Welcome to Snek!");
+                Console.WriteLine("****************");
+                Console.WriteLine(" The best game!");
+                Console.ReadKey();
+                // ask for player name
+
+                // run game loop
+
+                // print score
+
+                // ask play again
+                
+            } while (playAgain);
         }
     }
 }
