3b0862f1df3324dde618f5993abcb1404f197310
Linus Boréus <38762425+Swesen@users.noreply.github.com>
2021-02-08 19:59:03 +0100

    changing the scope of userInput

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace Snake
     class Program
     {
         // global variables
+        static int gameSpeed = 500;
 
         static void Main(string[] args)
         {
@@ -18,7 +19,7 @@ namespace Snake
                 // ask for player name
 
                 // run game loop
-                Game(500);
+                PlaySnake(gameSpeed);
 
                 // print score
 
@@ -32,12 +33,18 @@ namespace Snake
             Console.WriteLine("Welcome to Snek!");
             Console.WriteLine("****************");
             Console.WriteLine(" The best game! ");
+            Console.WriteLine("\nPress any key to start");
             Console.ReadKey();
         }
 
-        // gameLoopMS controls the time between updates
-        static void Game(int gameLoopMS)
+
+        /// <summary>
+        /// Main game, loops through the functions required to play the game.
+        /// </summary>
+        /// <param name="gameLoopMS">The time in milliseconds in between each game loop</param>
+        static void PlaySnake(int gameLoopMS)
         {
+            ConsoleKey userInput = new ConsoleKey();
             do
             {
                 // start timer to keep track of execution time
@@ -45,11 +52,16 @@ namespace Snake
 
                 // execute any game updating logick after this point
 
+                // check userInput if the snake is going to turn this update
 
-                // after every update read user input controls untill the next game update
-                ConsoleKey input = new ConsoleKey();
+
+                // keep this last
+                // loop read input controls until the next game update
+                // clear userInput from last game update
+                userInput = new ConsoleKey();
                 do
                 {
+
                     if (Console.KeyAvailable)
                     {
                         ConsoleKeyInfo key = Console.ReadKey(true);
@@ -58,16 +70,16 @@ namespace Snake
                         switch (key.Key)
                         {
                             case ConsoleKey.LeftArrow:
-                                input = key.Key;
+                                userInput = key.Key;
                                 break;
                             case ConsoleKey.RightArrow:
-                                input = key.Key;
+                                userInput = key.Key;
                                 break;
                             case ConsoleKey.UpArrow:
-                                input = key.Key;
+                                userInput = key.Key;
                                 break;
                             case ConsoleKey.DownArrow:
-                                input = key.Key;
+                                userInput = key.Key;
                                 break;
                             default:
                                 break;
@@ -75,14 +87,13 @@ namespace Snake
                     }
                 } while (timer.ElapsedMilliseconds < gameLoopMS);
 
-                // debug
-                if (input != ConsoleKey.Escape)
+                // debug input
+                if (userInput != ConsoleKey.Escape)
                 {
-                    Console.Write(input.ToString());
+                    Console.Write(userInput.ToString());
                 }
 
                 timer.Stop();
-
             } while (true);
         }
     }
