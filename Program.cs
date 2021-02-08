36c316aaa49e2dcad7fe193a8dce0db4808138c8
Linus Boréus <38762425+Swesen@users.noreply.github.com>
2021-02-08 20:05:46 +0100

    Feat/userinput snake control (#8)
    * Created a way of reading keypresses without pausing the game
    
    * changing the scope of userInput
    
    * Temporarially disabled debug

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Snake
 {
     class Program
     {
         // global variables
+        static int gameSpeed = 500;
 
         static void Main(string[] args)
         {
@@ -17,6 +19,7 @@ namespace Snake
                 // ask for player name
 
                 // run game loop
+                PlaySnake(gameSpeed);
 
                 // print score
 
@@ -30,7 +33,68 @@ namespace Snake
             Console.WriteLine("Welcome to Snek!");
             Console.WriteLine("****************");
             Console.WriteLine(" The best game! ");
+            Console.WriteLine("\nPress any key to start");
             Console.ReadKey();
+        }
+
+
+        /// <summary>
+        /// Main game, loops through the functions required to play the game.
+        /// </summary>
+        /// <param name="gameLoopMS">The time in milliseconds in between each game loop</param>
+        static void PlaySnake(int gameLoopMS)
+        {
+            ConsoleKey userInput = new ConsoleKey();
+            do
+            {
+                // start timer to keep track of execution time
+                var timer = System.Diagnostics.Stopwatch.StartNew();
+
+                // execute any game updating logick after this point
+
+                // check userInput if the snake is going to turn this update
+
+
+                // keep this last
+                // loop read input controls until the next game update
+                // clear userInput from last game update
+                userInput = new ConsoleKey();
+                do
+                {
+
+                    if (Console.KeyAvailable)
+                    {
+                        ConsoleKeyInfo key = Console.ReadKey(true);
+
+                        // only accept keys that are used in game
+                        switch (key.Key)
+                        {
+                            case ConsoleKey.LeftArrow:
+                                userInput = key.Key;
+                                break;
+                            case ConsoleKey.RightArrow:
+                                userInput = key.Key;
+                                break;
+                            case ConsoleKey.UpArrow:
+                                userInput = key.Key;
+                                break;
+                            case ConsoleKey.DownArrow:
+                                userInput = key.Key;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                } while (timer.ElapsedMilliseconds < gameLoopMS);
+
+                // debug input
+                //if (userInput != ConsoleKey.Escape)
+                //{
+                //    Console.Write(userInput.ToString());
+                //}
+
+                timer.Stop();
+            } while (true);
         }
     }
 }
