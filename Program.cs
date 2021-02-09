7dec96905da41a10be96e1b1b3fbe8030439ac94
Alejandra lopez pineda <al.lopezpineda@gmail.com>
2021-02-09 19:23:02 +0100

    Function move Snake without user input at the start the program

--- Program.cs
+++ Program.cs
@@ -40,14 +40,58 @@ namespace Snake
                 // run game loop
 
                 DrawVirtualGrid();
-
+                // test snake
+                virtualGameGrid[19, 21] = 1;
+                virtualGameGrid[20, 21] = 2;
+                virtualGameGrid[21, 21] = 3;
+                virtualGameGrid[22, 21] = 4;
+                virtualGameGrid[23, 21] = 5;
+                var position = MoveWithoutUserinput();
+                while (position > 1)
+                {
+                    position = MoveWithoutUserinput();
+                }
+                
                 PlaySnake(gameSpeed);
 
                 // print score
 
+
+
                 // ask play again
 
             } while (playAgain);
+        }
+
+        static int MoveWithoutUserinput()
+        {
+            int positionHead = 22;
+            for (int x = 0; x < virtualGameGrid.GetLength(0); x++)
+            {
+                for (int y = 0; y < virtualGameGrid.GetLength(1); y++)
+                {
+                    if(virtualGameGrid[x,y] == 1)
+                    {
+                        virtualGameGrid[x, y] = 0;
+                        virtualGameGrid[x+1, y] = 0;
+                        virtualGameGrid[x+2, y] = 0;
+                        virtualGameGrid[x+3, y] = 0;
+                        virtualGameGrid[x+4, y] = 0;
+
+                        virtualGameGrid[x-1, y] = 1;
+                        virtualGameGrid[x, y] = 2;
+                        virtualGameGrid[x+1, y] = 3;
+                        virtualGameGrid[x+2, y] = 4;
+                        virtualGameGrid[x+3, y] = 5;
+                        virtualGameGrid[x+4, y] = 0;
+                        Console.Clear();
+                        DrawVirtualGrid();
+                        positionHead = x;
+                    }
+
+                }
+            }
+            return positionHead;
         }
 
         static string GridContent(int gridValue)
@@ -67,12 +111,7 @@ namespace Snake
         private static void DrawVirtualGrid()
         {
             // some test values
-            // test snake
-            virtualGameGrid[20, 20] = 1;
-            virtualGameGrid[20, 21] = 2;
-            virtualGameGrid[21, 21] = 3;
-            virtualGameGrid[22, 21] = 4;
-            virtualGameGrid[23, 21] = 5;
+            
             // test food
             virtualGameGrid[10, 15] = -1;
 
@@ -139,8 +178,8 @@ namespace Snake
                 do
                 {
 
-                    if (Console.KeyAvailable)
-                    {
+                    //if (Console.KeyAvailable)
+                    //{
                         ConsoleKeyInfo key = Console.ReadKey(true);
 
                         // only accept keys that are used in game
@@ -161,7 +200,9 @@ namespace Snake
                             default:
                                 break;
                         }
-                    }
+                    //}
+
+
                 } while (timer.ElapsedMilliseconds < gameLoopMS);
 
                 // debug input
