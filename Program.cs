ffc0c9e995a48d55fde528eb405add18fabdfedc
Dmitri Iselund <dmitri.iselund@gmail.com>
2021-02-09 14:52:53 +0100

    Refined

--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ namespace Snake
             {
 
                 // welcome screen
-                //WelcomeScreen();
+                WelcomeScreen();
 
                 // ask for player name
 
